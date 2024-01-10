fbf78031739c2da6056c05ff37f605cbda7b078d
Gediminas Milašius <8672277+GedasFX@users.noreply.github.com>
2024-01-10 18:18:11 +0200

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 
 cd /backend
 
-wget https://downloads.rclone.org/v1.61.1/rclone-v1.61.1-linux-amd64.zip
+wget https://downloads.rclone.org/v1.65.1/rclone-v1.65.1-linux-amd64.zip
 
-checksum=$(md5sum rclone-v1.61.1-linux-amd64.zip)
-[ "$checksum" != "3d6893531ec9f2f1ed89ef9fe38dc468  rclone-v1.61.1-linux-amd64.zip" ] && exit 1
+checksum=$(md5sum rclone-v1.65.1-linux-amd64.zip)
+  [ "$checksum" != "d5585f0e81fdeeceb5a4277a4720ed99  rclone-v1.65.1-linux-amd64.zip" ] && exit 1
 
-unzip rclone-v1.61.1-linux-amd64.zip
+unzip rclone-v1.65.1-linux-amd64.zip
 
 mkdir -p out
-mv rclone-v1.61.1-linux-amd64/rclone out/
+mv rclone-v1.65.1-linux-amd64/rclone out/
