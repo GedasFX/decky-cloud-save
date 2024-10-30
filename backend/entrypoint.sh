7fa010a4589ab3985c8c72c0e33744382cd6ce3f
Snappi <8672277+GedasFX@users.noreply.github.com>
2024-10-30 09:42:00 +0200

    Update rclone

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,15 +3,15 @@ set -e
 
 cd /backend
 
-wget -nc https://downloads.rclone.org/v1.66.0/rclone-v1.66.0-linux-amd64.zip
+wget -nc https://downloads.rclone.org/v1.68.1/rclone-v1.68.1-linux-amd64.zip
 
-checksum=$(md5sum rclone-v1.66.0-linux-amd64.zip)
-[ "$checksum" != "59a19a5f31c258aacf99970ae7d028cd  rclone-v1.66.0-linux-amd64.zip" ] && exit 1
+checksum=$(md5sum rclone-v1.68.1-linux-amd64.zip)
+[ "$checksum" != "fff0fd5fdf25dd4a25e9464c3b1c7254  rclone-v1.68.1-linux-amd64.zip" ] && exit 1
 
-unzip -n rclone-v1.66.0-linux-amd64.zip
+unzip -n rclone-v1.68.1-linux-amd64.zip
 
 mkdir -p out
-mv rclone-v1.66.0-linux-amd64/rclone out/
+mv rclone-v1.68.1-linux-amd64/rclone out/
 
 cp rcloneLauncher out/
 cp openLastLog.sh out/
